33531d4029bb52d69a9d888077559dc63bfd96af
michaelsulistio <michaelsulistio@gmail.com>
2017-10-22 03:33:12 -0700

    testing

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1 // london is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
+
+exports.add = functions.https.onRequest((req, res) => {
+  // POST request to DB -- parse req
+
+
+
+
+
+  // redirect back to front page
+  res.status(200).send(
+
+
+
+  	);
+});
